7d94779be2e565927f0aa5e5e7667ae80634e7f7
William Pei Yuan <williampeiyuan@gmail.com>
2019-07-16 16:48:48 -0400

    Updates

---
+++ precache-manifest.19423dd151fee4d869018617b9abcd68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ba3094de3e29ca67e8be5a1d0b5c775",
+    "url": "/meetup-cf-tdd-app/index.html"
+  },
+  {
+    "revision": "ec212359495abe4dc9c2",
+    "url": "/meetup-cf-tdd-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/meetup-cf-tdd-app/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "ec212359495abe4dc9c2",
+    "url": "/meetup-cf-tdd-app/static/js/main.8798799f.chunk.js"
+  },
+  {
+    "revision": "fd3ca0e0cc89572cdba2",
+    "url": "/meetup-cf-tdd-app/static/js/runtime~main.0ae094fb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/meetup-cf-tdd-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
