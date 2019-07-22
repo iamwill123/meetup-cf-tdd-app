831f2367a03552c6e1da366ac31c8ff7b71b8ed1
William Pei Yuan <williampeiyuan@gmail.com>
2019-07-21 23:09:58 -0400

    UPDATES - refresh token update

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -2,15 +2,44 @@
 
 const axios = require('axios');
 
+const meetupClientId = 'j12j4mvc0gjql1p5is9iu8uib3';
+const meetupClientSecret = '3mqf089ejhk03nvikdr45702om';
 module.exports.getAccessToken = async event => {
-  console.log('TCL: event', event);
   const MEETUP_OAUTH_URL =
     'https://secure.meetup.com/oauth2/access' +
-    '?client_id=j12j4mvc0gjql1p5is9iu8uib3' +
-    '&client_secret=3mqf089ejhk03nvikdr45702om' +
+    '?client_id=' +
+    meetupClientId +
+    '&client_secret=' +
+    meetupClientSecret +
     '&grant_type=authorization_code' +
     '&redirect_uri=http://iamwill123.github.io/meetup-cf-tdd-app' +
     '&code=' +
+    event.pathParameters.code;
+
+  const info = await axios.post(MEETUP_OAUTH_URL);
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      access_token: info.data.access_token,
+      refresh_token: info.data.refresh_token
+    })
+  };
+};
+
+module.exports.getRefreshAccessToken = async event => {
+  const MEETUP_OAUTH_URL =
+    'https://secure.meetup.com/oauth2/access' +
+    '?client_id=' +
+    meetupClientId +
+    '&client_secret=' +
+    meetupClientSecret +
+    '&grant_type=refresh_token' +
+    '&refresh_token=' +
     event.pathParameters.code;
 
   const info = await axios.post(MEETUP_OAUTH_URL);
