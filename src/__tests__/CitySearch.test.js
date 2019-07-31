c90dd990b11aa73027a3632b544e2328ff8cdca7
William Pei Yuan <williampeiyuan@gmail.com>
2019-07-30 22:49:46 -0400

    4.3 Unit testing - Scenario 2: Show a List of Suggestions During Search - 100%

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -15,4 +15,29 @@ describe('<CitySearch /> component', () => {
   test('should render list of suggestions', () => {
     expect(CitySearchWrapper.find('.suggestions')).toHaveLength(1);
   });
+
+  test('should render text input correctly', () => {
+    const query = CitySearchWrapper.state('query');
+    expect(CitySearchWrapper.find('.city').prop('value')).toBe(query);
+  });
+
+  test('should change state when text input changes', () => {
+    const eventObject = { target: { value: 'Brooklyn' } };
+    CitySearchWrapper.find('.city').simulate('change', eventObject);
+    expect(CitySearchWrapper.state('query')).toBe('Brooklyn');
+  });
+
+  test('render list of suggestions correctly', () => {
+    const suggestions = CitySearchWrapper.state('suggestions');
+    expect(CitySearchWrapper.find('.suggestions li')).toHaveLength(
+      suggestions.length
+    );
+    for (let i = 0; i < suggestions.length; i++) {
+      expect(
+        CitySearchWrapper.find('.suggestions li')
+          .at(i)
+          .text()
+      ).toBe(suggestions[i].name_string);
+    }
+  });
 });
--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -1,11 +1,132 @@
 import React, { Component } from 'react';
 
 class CitySearch extends Component {
+  state = {
+    query: '',
+    suggestions: [
+      {
+        city: 'Brooklyn',
+        country: 'us',
+        localized_country_name: 'USA',
+        state: 'NY',
+        name_string: 'Brooklyn, New York, USA',
+        zip: '11201',
+        lat: 40.7,
+        lon: -73.99
+      },
+      {
+        city: 'Brooklyn Heights',
+        country: 'us',
+        localized_country_name: 'USA',
+        state: 'OH',
+        name_string: 'Brooklyn Heights, Ohio, USA',
+        zip: '44131',
+        lat: 41.38,
+        lon: -81.66
+      },
+      {
+        city: 'Brooklyn Heights',
+        country: 'us',
+        localized_country_name: 'USA',
+        state: 'MO',
+        name_string: 'Brooklyn Heights, Missouri, USA',
+        zip: '64836',
+        lat: 37.17,
+        lon: -94.31
+      },
+      {
+        city: 'Brooklyn',
+        country: 'us',
+        localized_country_name: 'USA',
+        state: 'MD',
+        name_string: 'Brooklyn, Maryland, USA',
+        zip: '21225',
+        lat: 39.23,
+        lon: -76.62
+      },
+      {
+        city: 'Brooklyn',
+        country: 'us',
+        localized_country_name: 'USA',
+        state: 'MN',
+        name_string: 'Brooklyn, Minnesota, USA',
+        zip: '55746',
+        lat: 47.42,
+        lon: -92.93
+      },
+      {
+        city: 'Brooklyn',
+        country: 'us',
+        localized_country_name: 'USA',
+        state: 'CT',
+        name_string: 'Brooklyn, Connecticut, USA',
+        zip: '06234',
+        lat: 41.79,
+        lon: -71.95
+      },
+      {
+        city: 'Brooklyn',
+        country: 'us',
+        localized_country_name: 'USA',
+        state: 'MI',
+        name_string: 'Brooklyn, Michigan, USA',
+        zip: '49230',
+        lat: 42.1,
+        lon: -84.25
+      },
+      {
+        city: 'Brooklyn',
+        country: 'us',
+        localized_country_name: 'USA',
+        state: 'KY',
+        name_string: 'Brooklyn, Kentucky, USA',
+        zip: '42261',
+        lat: 37.2,
+        lon: -86.69
+      },
+      {
+        city: 'Brooklyn',
+        country: 'us',
+        localized_country_name: 'USA',
+        state: 'WI',
+        name_string: 'Brooklyn, Wisconsin, USA',
+        zip: '53521',
+        lat: 42.87,
+        lon: -89.39
+      },
+      {
+        city: 'Brooklyn',
+        country: 'us',
+        localized_country_name: 'USA',
+        state: 'IA',
+        name_string: 'Brooklyn, Iowa, USA',
+        zip: '52211',
+        lat: 41.73,
+        lon: -92.45
+      }
+    ]
+  };
+
+  handleInputChange = event => {
+    const value = event.target.value;
+    this.setState({ query: value });
+  };
+
   render() {
+    const { query, suggestions } = this.state;
     return (
       <div className="CitySearch">
-        <input type="text" className="city" />
-        <ul className="suggestions" />
+        <input
+          type="text"
+          className="city"
+          value={query}
+          onChange={this.handleInputChange}
+        />
+        <ul className="suggestions">
+          {suggestions.map(item => (
+            <li key={item.zip}>{item.name_string}</li>
+          ))}
+        </ul>
       </div>
     );
   }
