38ff850a70f1bfdc1a70b14a175307077c45e54b
William Pei Yuan <williampeiyuan@gmail.com>
2019-08-09 14:14:13 -0400

    4.3 Unit Testing - Event and NumberOfEvents tests - 2

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -3,7 +3,10 @@ import { shallow } from 'enzyme';
 import App from '../App';
 // import your new EventList component into your test so that you’re able to find it via the shallow rendering API
 import EventList from '../components/EventList';
+
 import CitySearch from '../components/CitySearch';
+import Event from '../components/Event';
+import NumberOfEvents from '../components/NumberOfEvents';
 
 // Basic list of functions we can use:
 // find(selector): locates every node that matches the selector (a CSS selector, a component constructor, etc). It returns another wrapper surrounding any nodes it finds. You can then call additional functions on this new wrapper.
@@ -18,6 +21,7 @@ import CitySearch from '../components/CitySearch';
 // create a new group, or “scope” for test
 describe('<App /> component', () => {
   let AppWrapper;
+
   beforeAll(() => {
     AppWrapper = shallow(<App />);
   });
@@ -28,5 +32,9 @@ describe('<App /> component', () => {
 
   test('should render CitySearch', () => {
     expect(AppWrapper.find(CitySearch)).toHaveLength(1);
+  });
+
+  test('should render NumberOfEvents', () => {
+    expect(AppWrapper.find(NumberOfEvents)).toHaveLength(1);
   });
 });
--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -24,28 +24,4 @@ describe('<NumberOfEvents /> component', () => {
     expect(NumberOfEventsWrapper.state('query')).toBe('23');
   });
 
-  // test('should render list of suggestions correctly', () => {
-  //   const suggestions = NumberOfEventsWrapper.state('suggestions');
-  //   expect(NumberOfEventsWrapper.find('.suggestions li')).toHaveLength(
-  //     suggestions.length
-  //   );
-  //   for (let i = 0; i < suggestions.length; i++) {
-  //     expect(
-  //       NumberOfEventsWrapper.find('.suggestions li')
-  //         .at(i)
-  //         .text()
-  //     ).toBe(suggestions[i].name_string);
-  //   }
-  // });
-
-  // test('click on suggestion should change query state', () => {
-  //   NumberOfEventsWrapper.setState({
-  //     suggestions: suggestionsData
-  //   });
-
-  //   NumberOfEventsWrapper.find('.suggestions li')
-  //     .at(0)
-  //     .simulate('click');
-  //   expect(NumberOfEventsWrapper.state('query')).toBe('Brooklyn, New York, USA');
-  // });
 });
