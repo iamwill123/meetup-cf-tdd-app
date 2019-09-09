9f58da17790214146ae9d0ae49b52d01676041f5
William Pei Yuan <williampeiyuan@gmail.com>
2019-09-09 12:25:29 -0400

    4.8 Enable service worker

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
 
 // https://www.atatus.com/
 atatus.config('80bd1fedf2084833b437750e18d7552a').install();
